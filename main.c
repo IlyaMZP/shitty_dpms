4ceaa73e72cbc86898c25ecb3e4e1d0d050c94c0
IlyaMZP <vasya.barashkin@gmail.com>
2018-08-16 06:31:53 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,97 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/sysinfo.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+long timer, prevtime;
+bool reset;
+
+void read_input(int byteAmount, char *infile)
+{
+	char ch;
+	int count = 0;
+	FILE *fp;
+	fp = fopen(infile, "r");
+	if( fp == NULL )
+	{
+		fprintf(stderr, "Error while opening the file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while( ( ch = fgetc(fp) ) != EOF || count < byteAmount)
+	{
+		count++;
+	}
+	fclose(fp);
+}
+
+void *mice_thread(void *p)
+{
+	while(1)
+	{
+		read_input(1, "/dev/input/mice");
+		reset = true;
+		sleep(1);
+	}
+}
+
+void *keyboard_thread(void *p)
+{
+        while(1)
+        {
+                read_input(1, p);
+                reset = true;
+                sleep(1);
+        }
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		fprintf(stderr, "usage: dpms <OffTime> <Path to keyboard dev>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	uid_t uid=getuid();
+	if (uid != 0)
+	{
+		fprintf(stderr, "Should be run as root\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int OffTime = atoi(argv[1]);
+	char *keyboard_path = argv[2];
+
+	struct sysinfo s_info;
+	sysinfo(&s_info);
+	prevtime = s_info.uptime;
+
+	pthread_t thread1, thread2;
+	int  ret1, ret2;
+
+	ret1 = pthread_create(&thread1, NULL, mice_thread, NULL);
+	ret2 = pthread_create(&thread2, NULL, keyboard_thread, (char*) keyboard_path);
+
+	while(1)
+        {
+                sysinfo(&s_info);
+                timer = s_info.uptime - prevtime;
+
+                if (reset)
+                {
+                        prevtime = s_info.uptime;
+                        reset = false;
+                }
+
+                if (timer >= OffTime)
+                        system("xset dpms force off  &>/dev/null");
+
+                sleep(10);
+        }
+	return(0);
+}
+
